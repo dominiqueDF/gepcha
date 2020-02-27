c1d285292b6f633950aa738440d11263af0e5fcf
Dominique Rigaudiere <d-rigaudiere@dijonformation.com>
2020-02-27 11:46:24 +0100

    Formulaires CRUD Owner

--- src/Form/OwnerType.php
+++ src/Form/OwnerType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Owner;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,15 +13,55 @@ class OwnerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('lastName')
-            ->add('firstName')
-            ->add('email')
-            ->add('phoneNumber')
-            ->add('addressLine1')
-            ->add('addressLine2')
-            ->add('city')
-            ->add('postCode')
+            ->add('title', ChoiceType::class, [
+                'required' => true,
+                'choices' => [
+                    'Civilité' => 'Civilité',
+                    'Monsieur' => 'Monsieur',
+                    'Madame' => 'Madame'
+                ],
+                'empty_data' => 'Civilité'
+            ])
+            ->add('lastName', null, [
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ]
+            ])
+            ->add('firstName', null, [
+                'attr' => [
+                    'placeholder' => 'Prénom'
+                ]
+            ])
+            ->add('email', null, [
+                'attr' => [
+                    'placeholder' => 'email'
+                ]
+            ])
+            ->add('phoneNumber', null, [
+                'attr' => [
+                    'placeholder' => 'Téléphone'
+                ]
+            ])
+            ->add('addressLine1', null, [
+                'attr' => [
+                    'placeholder' => 'Adresse'
+                ]
+            ])
+            ->add('addressLine2', null, [
+                'attr' => [
+                    'placeholder' => 'Complément d\'adresse'
+                ]
+            ])
+            ->add('city', null, [
+                'attr' => [
+                    'placeholder' => 'Ville'
+                ]
+            ])
+            ->add('postCode', null, [
+                'attr' => [
+                    'placeholder' => 'Code postal'
+                ]
+            ])
         ;
     }
 
